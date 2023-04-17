6e5e50dd78cf0ab22d43113e0c51747e6b306dd9
Khachatur-Khachatryan <xachulxx@gmail.com>
2023-04-17 08:39:14 +0300

    feature: initialized minimal application

--- src/client/BarkditorGui.BusinessLogic/GtkWidgets/Windows/NewMainWindow.cs
+++ src/client/BarkditorGui.BusinessLogic/GtkWidgets/Windows/NewMainWindow.cs
@@ -1,6 +1,15 @@
+using Gtk;
+
 namespace BarkditorGui.BusinessLogic.GtkWidgets.Windows;
 
-public class NewMainWindow
+public class NewMainWindow : Window
 {
-    
+    private NewMainWindow(Builder builder, string name) : base(builder.GetPointer(name), false)
+    {
+        builder.Connect(this);
+    }
+
+    public NewMainWindow() : this(new Builder("helloworld.ui"), "MainWindow")
+    {
+    }
 }
--- src/client/BarkditorGui.Presentation/NewProgram.cs
+++ src/client/BarkditorGui.Presentation/NewProgram.cs
@@ -1,9 +1,22 @@
+using System;
+using BarkditorGui.BusinessLogic.GtkWidgets.Windows;
+using Gtk;
+
 namespace BarkditorGui.Presentation;
 
 public static class NewProgram
 {
+    [STAThread]
     public static void Main(string[] args)
     {
-        
+        var application = Application.New("org.HelloWorld.core", Gio.ApplicationFlags.FlagsNone);
+        application.OnActivate += (sender, _) =>
+        {
+            var window = new NewMainWindow();
+            window.Application = application;
+            window.Show();
+            window.OnDestroy += (_, _) => application.Quit();
+        };
+        application.Run();
     }
 }
