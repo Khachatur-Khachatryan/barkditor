e3aa783a442d65315edd1a5cf8582aa10c9bb017
Хачатур <xachulxx@icloud.com>
2023-04-19 11:38:41 +0300

    client cleanup

--- src/client/BarkditorGui.Presentation/Program.cs
+++ src/client/BarkditorGui.Presentation/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Threading.Tasks;
-using BarkditorGui.BusinessLogic.GtkWidgets.Windows;
-using GLib;
-using Application = Gtk.Application;
 
 namespace BarkditorGui.Presentation;
 
@@ -11,22 +7,6 @@ public static class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        Application.Init();
-
-        var app = new Application("org.BarkditorGui.Presentation", ApplicationFlags.None);
-        app.Register(Cancellable.Current);
-
-        var win = new MainWindow();
-        app.AddWindow(win);
-
-        async void Action()
-        {
-            await win.FileSystemViewer.StartWatching();
-        }
-
-        Parallel.Invoke(Action);
         
-        win.Show();
-        Application.Run();
     }
 }
